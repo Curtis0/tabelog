9eadf935125f3fd72c5dc0f5b6dd0c6a6134dbd8
Seiya Bryant <seiyathesaint@gmail.com>
2015-06-27 19:36:38 +0900

    added before_action by LOOKING ONLINE for help. added create by myself including the shop_params after the Shop.new. added AddShop link to header successfully linking to new shop page. created show.html.erb and successfully showed the name of the restaurant I added as a user.

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,5 @@
 class ShopsController < ApplicationController
-
+before_action :set_before, only: [ :edit, :update, :destroy, :show ]
   def index
     @shop = Shop.new
   end
@@ -8,6 +8,35 @@ class ShopsController < ApplicationController
     @shop = Shop.new
   end
 
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to @shop
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+
+
+  def set_before
+    @shop = Shop.find(params[:id])
+  end
+
+
+private
+
+  def shop_params
+    params.require(:shop).permit(
+      :name,
+      :phone_number,
+      :address,
+      :shop_url
+      )
+  end
 
 
 end
