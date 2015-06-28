45b78710bdcff2b13b8e38cd7231b0a8d035c96c
Seiya Bryant <seiyathesaint@gmail.com>
2015-06-28 19:37:08 +0900

    changed before action

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ActiveRecord::Base
   validates :score, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :user_id, :uniqueness => { :scope => :product_id,
+    :message => "Users may only write one review per product." }
   belongs_to :user
   belongs_to :shop
 end
